9411f23cd7b0f36cb3172c46c0b18c6be1e7a054
Nandkishor Ban <nban@netchexonline.com>
2019-12-16 15:48:37 +0530

    updation in matrix prog.

--- src/Automation_Framework/Matrix.java
+++ src/Automation_Framework/Matrix.java
@@ -3,23 +3,37 @@ package Automation_Framework;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+
+import java.util.Scanner;
+
 import java.util.Map.Entry;
 
 public class Matrix {
 
 	public static void main(String[] args) {
-		int rows =3;
-		int columns =3;
+			
+		 System.out.print("Enter 2D array size : ");
+	       Scanner sc = new Scanner(System.in);
+	       int rows=sc.nextInt();
+	       int columns = sc.nextInt();
+	       
+	       HashMap<Integer, Integer> Rowsum 
+	        = new HashMap<Integer, Integer>();   
+	       
+	       System.out.println("Enter array elements : ");    
 		
-		HashMap<Integer, Integer> Rowsum 
-        = new HashMap<Integer, Integer>();   
+	//	int[][] firstMatrix = { {2, 3, 9},{5, 2, 9},{5, 3, 9} };
 		
-		int[][] firstMatrix = { {2, 3, 9},
-				                {5, 2, 9},
-				                {5, 3, 9}	                		                 
-				              };
-  
-       
+		int  firstMatrix[][]  = new int [rows][columns];
+		
+		  for(int i=0; i<rows;i++)
+	         {            
+	            for(int j=0; j<columns;j++)
+	            {
+	            	firstMatrix[i][j]=sc.nextInt();
+	            }
+	         }
+        
              for(int i = 0; i < rows; i++) {
         	
         	  int sum=0;
@@ -56,8 +70,8 @@ public class Matrix {
             	maxsum= entry.getValue();	                 
                 maxrow  = entry.getKey();
             }
-
-        }	        
+        }	
+        
         System.out.println();
         System.out.println(maxrow + " " + maxsum); 
         
